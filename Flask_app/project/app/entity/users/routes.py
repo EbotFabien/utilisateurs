2503390d5ba476953bc96c4564fa62e406f02404
root <root@ov-b123cd.ch2.infomaniak.ch>
2023-04-18 11:26:13 +0000

    done

--- Flask_app/project/app/entity/users/routes.py
+++ Flask_app/project/app/entity/users/routes.py
@@ -12,7 +12,7 @@ agent_sec = db.collection('Agentsec')
 
 users =Blueprint('users',__name__)
 
-@cross_origin(origin=["http://127.0.0.1","http://195.15.228.250","*"],headers=['Content- Type','Authorization'])
+@cross_origin(origin=["http://127.0.0.1:5274","http://195.15.228.250","*"],headers=['Content-Type','Authorization'])
 @users.route('/Agentsec/ajouter', methods=['POST'])
 def create():
     try:
@@ -32,13 +32,13 @@ def create():
     else:
         return 400
 
-@cross_origin(origin=["http://127.0.0.1","http://195.15.228.250","*"],headers=['Content- Type','Authorization'])
+@cross_origin(origin=["http://127.0.0.1","http://195.15.228.250","*"],headers=['Content-Type','Authorization'])
 @users.route('/Agentsec/tous', methods=['GET'])
 def read():
     all_todos = [doc.to_dict() for doc in agent_sec.stream()]
     return jsonify(all_todos), 200
 
-@cross_origin(origin=["http://127.0.0.1","http://195.15.228.250","*"],headers=['Content- Type','Authorization'])
+@cross_origin(origin=["http://127.0.0.1","http://195.15.228.250","*"],headers=['Content-Type','Authorization'])
 @users.route('/Agentsec/<int:ide>', methods=['GET'])
 def read_ind(ide):
     todo_id = str(ide)
@@ -50,7 +50,7 @@ def read_ind(ide):
         else:
             return jsonify(todo.to_dict()), 200
 
-@cross_origin(origin=["http://127.0.0.1","http://195.15.228.250","*"],headers=['Content- Type','Authorization'])
+@cross_origin(origin=["http://127.0.0.1","http://195.15.228.250","*"],headers=['Content-Type','Authorization'])
 @users.route('/Agentsec/update/<int:ide>', methods=['POST', 'PUT'])
 def update(ide):
         todo_id = str(ide)
@@ -61,7 +61,7 @@ def update(ide):
             agent_sec.document(todo_id).update(request.json)
             return jsonify({"success": True}), 200
 
-@cross_origin(origin=["http://127.0.0.1","http://195.15.228.250","*"],headers=['Content- Type','Authorization'])
+@cross_origin(origin=["http://127.0.0.1","http://195.15.228.250","*"],headers=['Content-Type','Authorization'])
 @users.route('/Agentsec/delete/<int:ide>', methods=['GET', 'DELETE'])
 def delete(ide):
     todo_id = str(ide)
